6acbf12459e907016d6ec9a142609cba748d6cc6
DoomedPersonDood <42052759+DoomedPersonDood@users.noreply.github.com>
2018-08-03 15:32:13 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,7 +15,7 @@ client.on('message', message => {
 
             
             let Role = message.author.guild.roles.find('name', 'PC');
-            message.reply('there');
+            return message.reply('here');
             if (Role) {
                 message.reply('here');
                 if (!message.author.roles.has(Role.id)) {
