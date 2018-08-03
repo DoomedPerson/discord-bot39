743d90cf2e510777177fa42bf4382bb0b9e7ddf4
DoomedPersonDood <42052759+DoomedPersonDood@users.noreply.github.com>
2018-08-03 15:04:08 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,19 +11,22 @@ client.on('message', message => {
     	message.reply("Type '!PC-Player+' if your a PC player, '!XBox-Player+' if your a XBox player, '!PS4-Player+' if your a PS4 player, and '!Switch-Player+' if your a switch player. To remove your role type '!*platform*_Player-'");
   	}
     if (messagecontent === "!pc-player+") {
-        message.reply("i get here")
+        let newstring = substr(messagecontent, 0, 3);
+        if (newstring === "!pc") {
+            message.reply("i get here");
 
             
-            let platform = "PC"
-            let Role = message.guild.roles.find('name', platform)
+            let platform = "PC";
+            let Role = message.guild.roles.find('name', platform);
             
-            if (!Role) return message.reply("Uh Oh, an unknown error has occurred. Please try again")
+            if (!Role) return message.reply("Uh Oh, an unknown error has occurred. Please try again");
             
             if (Role) {
                 if (!message.author.roles.has(Role.id)) {
                     message.author.addRole(Role.id);
                 }
             }
+        }
     }
 });
 
