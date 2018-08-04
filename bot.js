6edc9a75f987543671a954a5f275985967832e13
DoomedPersonDood <42052759+DoomedPersonDood@users.noreply.github.com>
2018-08-04 12:41:31 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log('I am ready!');
-    
+    return message.reply('thereeeee');
     bot.user.setActivity("JavaScript..ing?")
 });
 
