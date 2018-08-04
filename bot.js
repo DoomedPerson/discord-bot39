4fae4bc45c3fc5d98c7c2e6e1c5c6897a8d77806
DoomedPersonDood <42052759+DoomedPersonDood@users.noreply.github.com>
2018-08-04 13:49:58 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const client = new Discord.Client();
 client.on('ready', function() { 
     console.log('I am ready!');
     
-    client.setActivity('JavaScript..ing?')
+    client.user.setActivity('YouTube', { type: 'WATCHING' });
     
 });
 
