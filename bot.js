4507ea3e2f733525da15614ca2187afd511db563
DoomedPersonDood <42052759+DoomedPersonDood@users.noreply.github.com>
2018-08-03 20:23:08 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ client.on('ready', () => {
 
 client.on('message', message => {
     let messagecontent = message.content.toLowerCase();
-    return message.reply(messagecontent)
+    
     if (messagecontent === '!platform-commands') {
     	message.reply("Type '!PC-Player+' if your a PC player, '!XBox-Player+' if your a XBox player, '!PS4-Player+' if your a PS4 player, and '!Switch-Player+' if your a switch player. To remove your role type '!*platform*_Player-'");
   	}
