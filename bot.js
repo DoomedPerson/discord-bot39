cca5be9bfcbaf2d2fbb415837f7b62841bd9d5d9
DoomedPersonDood <42052759+DoomedPersonDood@users.noreply.github.com>
2018-08-04 13:33:37 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,14 +4,14 @@ const client = new Discord.Client();
 client.on('ready', function() { 
     console.log('I am ready!');
 
-    client.user.setGame("JavaScript..ing?")
+    
 });
 
 client.on('message', message => {
     let messagecontent = message.content.toLowerCase();
     
     if (messagecontent === '!platform-commands') {
-    	message.reply("Type '!PC-Player+' if your a PC player, '!XBox-Player+' if your a XBox player, '!PS4-Player+' if your a PS4 player, and '!Switch-Player+' if your a switch player. To remove your role type '!*platform*_Player-'");
+    	message.reply("ahhhhh Type '!PC-Player+' if your a PC player, '!XBox-Player+' if your a XBox player, '!PS4-Player+' if your a PS4 player, and '!Switch-Player+' if your a switch player. To remove your role type '!*platform*_Player-'");
     }
     
     if (messagecontent === '!pc-player+') {
