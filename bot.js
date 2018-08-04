b37cab434c4467397d594b7485e2ca8e0e2f4721
DoomedPersonDood <42052759+DoomedPersonDood@users.noreply.github.com>
2018-08-04 14:20:19 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ client.on('ready', () => {
     client.user.setStatus('idle') // Can be 'available', 'idle', 'dnd', or 'invisible'
     client.user.setPresence({
         game: {
-            name: 'you.',
+            name: 'you. o.0',
             type: 3
         }
     });
